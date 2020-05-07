52db6dcd0f3a4f8f8d8bd8a8ea2975dec88aaf69
IsaacXu <xuhonggang@hotmail.com>
2020-05-07 23:04:13 +0800

    ....

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './header/Header'
-import List from './list/List'
+import Header from './Header/Header'
+import List from './List/List'
 function App() {
   return (
     <div className="App">
